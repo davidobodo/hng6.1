a7ef70f84be56fabd6ae1434eafb884bc7a6dfec
David Obodo <obododavid5@gmail.com>
2019-09-18 08:22:38 +0100

    added some extra styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const displaySignup = () => {
     document.querySelector('.sign_in').style.display = 'none'
     document.querySelector('.sign_up').style.display = 'flex'
+    document.querySelector('.sign_up').style.transition = 'display 1s .4s'
     document.querySelector('.sign_up').style.width = '100%'
 }
 document.querySelector('.sign_in>a').addEventListener('click', displaySignup)
